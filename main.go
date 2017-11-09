1c765dfa4e3e8a85ab8a388d46d33d7f5bd792a7
Maciej Gruszka <maciej.gruszka@oracle.com>
2017-11-08 19:14:49 -0800

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!! - test"))
+	w.Write([]byte("Hello World!! - test on Wednesday"))
 }
 
 func main() {
